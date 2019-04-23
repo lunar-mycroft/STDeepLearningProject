7545fa785749fb09b2bf2597e1aaf251d9637a15
lgreath5 <lgreath5@kent.edu>
2019-04-23 15:53:08 -0400

    Added python file to reformat data

---
+++ dataReformatter.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Read in original data
+df = pd.read_csv('originalData.csv', sep=',')
+
+# Figure out cutoff for what will be test/train data
+mostRecentTimestamp = max(df.timestamp)
+testTrainCutOff = mostRecentTimestamp - 86400000 # 24 hours = 86 400 000 milliseconds
+
+# Split the data into test and train sets based on timestamp
+trainDf = df[(df['timestamp'] < testTrainCutOff)]
+testDf = df[(df['timestamp'] >= testTrainCutOff)]
+
+# Use this code if we want separate counts for each event
+finalTrainDf = trainDf.groupby(['visitorid','itemid', 'event']).size().reset_index().rename(columns={0:'count'})
+finalTestDf = testDf.groupby(['visitorid','itemid', 'event']).size().reset_index().rename(columns={0:'count'})
+
+# Use this code if we want cumulative count including all events
+#finalTrainDf = trainDf.groupby(['visitorid','itemid']).size().reset_index().rename(columns={0:'count'})
+#finalTestDf = testDf.groupby(['visitorid','itemid']).size().reset_index().rename(columns={0:'count'})
+
+# Save test/train data to CSV files to be uploaded later
+finalTrainDf.to_csv (r'trainData.csv', index = None, header=True)
+finalTestDf.to_csv (r'testData.csv', index = None, header=True)
